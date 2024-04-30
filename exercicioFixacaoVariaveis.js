8e4c4c3650d04595c05de66ac8e3a4e13cddcf06
Karina Gante <karina.g@aluno.ifsp.edu.br>
2024-04-30 17:26:53 -0300

    Exercício Fixação Variáveis JS

---
+++ exercicioFixacaoVariaveis.js
@@ -0,0 +1,20 @@
+//nome, idade, num de telefone, cpf, endereço, recebe pensao
+
+let nome = "Jose"
+let idade = 115
+
+//Boa prática: todo valor numérico que não for realizar operação matemática poseriormente, recomenda-se armazenar como string
+
+let numTelefone = "(11)98471-3388"
+let cpf = "312.122.312-45"
+
+let endereco = "Rua dos Limoeiros, 75"
+let recebePensao = true
+
+//caracteristicas mouse: modelo, marca, peso, dpi, possui RGB
+let modelo = "viper mini"
+let marca = "razer"
+let peso = 49
+let dpi = 30000
+let possuiRgb = true
+
