fc65fbd10c4d8e32e70d8f5416318a9763a0b5b5
Karina Gante <karina.g@aluno.ifsp.edu.br>
2024-05-31 19:39:41 -0300

    Detecção de evento do clique do mouse com Godot

---
+++ Game Design com Godot/Simulação Física e Colisão/scripts/gameManager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	# check if event is a click / verificar se o evento é um click
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			if event.pressed:
+					print(event)
+	pass	
