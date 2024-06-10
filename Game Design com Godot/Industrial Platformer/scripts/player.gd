81be66d44f42d05b1bc0c564f7cfbd9865f02d7c
Karina Gante <karina.g@aluno.ifsp.edu.br>
2024-06-10 18:01:49 -0300

    Melhorando e suavizando a física do Player

--- Game Design com Godot/Industrial Platformer/scripts/player.gd
+++ Game Design com Godot/Industrial Platformer/scripts/player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+@export var speed = 300.0
+@export_range(0, 1) var lerpFactor = 0.5
+
+@onready var sprite = %Sprite
 
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * SPEED
-
+	var targetVelocity = direction * speed * 100.0
+	velocity = lerp(velocity, targetVelocity, lerpFactor)
 	move_and_slide()
+	
+	var targetRotation = direction.x * 45.0
+	sprite.rotation_degrees= lerp(sprite.rotation_degrees, targetRotation, lerpFactor)
