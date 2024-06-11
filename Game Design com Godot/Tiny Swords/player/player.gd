3c6da8921683fc8058fcaf845ee9738d0878562b
Karina Gante <karina.g@aluno.ifsp.edu.br>
2024-06-11 17:25:30 -0300

    Criando ações com InputMap

--- Game Design com Godot/Tiny Swords/player/player.gd
+++ Game Design com Godot/Tiny Swords/player/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 var isRunning: bool = false
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("move_up"):
 		if isRunning:
 			animationPlayer.play("idle")
 			isRunning = false
