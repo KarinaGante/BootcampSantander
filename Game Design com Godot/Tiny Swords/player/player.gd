da866015bd8dbdf517f25f383fead8927abb5b3a
Karina Gante <karina.g@aluno.ifsp.edu.br>
2024-06-11 18:47:44 -0300

    Animação de ataque

--- Game Design com Godot/Tiny Swords/player/player.gd
+++ Game Design com Godot/Tiny Swords/player/player.gd
@@ -6,6 +6,17 @@ extends CharacterBody2D
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 var isRunning: bool = false
+var isAttacking: bool = false
+var attackCooldown: float = 0.0
+
+func _process(delta: float) -> void: 
+	# atualizar tempo limite p/ proximo ataque
+	if isAttacking:
+		attackCooldown -= delta
+		if attackCooldown <= 0.0:
+			isAttacking = false
+			isRunning = false
+			animationPlayer.play("idle")
 
 # a funcao process atualiza em uma velocidade que depende do hardware
 # a funcao physics process vai atualizar com um valor fixo independente do harware (+ confiavel)
@@ -24,6 +35,10 @@ func _physics_process(delta: float) -> void:
 	
 	# modificar velocidade
 	var targetVelocity = inputVector * speed * 100.0
+	
+	if isAttacking:
+		targetVelocity *= 0.25
+	
 	velocity = lerp(velocity, targetVelocity, 0.05)
 	move_and_slide()
 	
@@ -32,11 +47,12 @@ func _physics_process(delta: float) -> void:
 	isRunning = not inputVector.is_zero_approx()
 	
 	# tocar animacao
-	if wasRunning != isRunning:
-		if isRunning:
-			animationPlayer.play("run")
-		else:
-			animationPlayer.play("idle")
+	if not isAttacking:		
+		if wasRunning != isRunning:
+			if isRunning:
+				animationPlayer.play("run")
+			else:
+				animationPlayer.play("idle")
 			
 	# girar sprite
 	if inputVector.x > 0:
@@ -46,3 +62,23 @@ func _physics_process(delta: float) -> void:
 	elif inputVector.x < 0:
 		# marcar flip_H
 		sprite.flip_h = true
+		
+	# ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
+		pass
+		
+func attack() -> void:
+	# ataque side1
+	# ataque side2
+	if isAttacking:
+		return
+		
+	# tocar animacao 
+	animationPlayer.play("attack_side1")
+	
+	# configurar temporizador
+	attackCooldown = 0.6
+	
+	# marcar ataque
+	isAttacking = true
